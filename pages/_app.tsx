d8899f0a7d6d6dba72221e8edc66b68d96ee892b
paulpladziewicz <ppladziewicz@gmail.com>
2022-06-12 08:19:55 -0400

    added redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
 import type { AppProps } from 'next/app';
+import { Provider } from 'react-redux'
+import store from 'redux-toolkit/store';
 import 'styles/tailwind.css';
 import 'styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+      <Provider store={store}>
+        <Component {...pageProps} />)
+      </Provider>
+  );
 }
 
 export default MyApp;
